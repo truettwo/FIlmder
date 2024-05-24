634ebb529c4cef476d537009765e9383377dbbab
truettwo <fedor.ivanov2009@mail.ru>
2024-05-24 18:27:08 +0300

    v1

---
+++ app/src/main/java/com/example/vv1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.vv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+}
